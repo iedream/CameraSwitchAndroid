b73ab39096d8bcb0f06554cbb4fa6615f27c4260
Catherine Zhao <iedream@Catherines-MacBook-Pro.local>
2017-06-02 23:20:13 -0400

    add more methods to storingmanager

--- app/src/main/java/com/example/iedream/cameraswitchandroid/StoringManager.java
+++ app/src/main/java/com/example/iedream/cameraswitchandroid/StoringManager.java
@@ -5,7 +5,12 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.nestlabs.sdk.Camera;
 import com.nestlabs.sdk.NestToken;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
 
 /**
  * Created by iedream on 2017-05-31.
@@ -34,6 +39,35 @@ public class StoringManager {
         SharedPreferences.Editor editor = settings.edit();
         editor.putString("token", token.getToken());
         editor.putLong("ExpiresIn", token.getExpiresIn());
+        editor.apply();
+    }
+
+    public Proximity readProximitySettings(String cameraId) {
+        SharedPreferences settings = context.getApplicationContext().getSharedPreferences("CameraSwitch",0);
+        int proximityInt = settings.getInt(cameraId, 0);
+        Proximity proximity = Proximity.values()[proximityInt];
+        return proximity;
+    }
+
+    public void writeProximitySetting(CameraModel camera) {
+        SharedPreferences settings = context.getApplicationContext().getSharedPreferences("CameraSwitch",0);
+        SharedPreferences.Editor editor = settings.edit();
+        String key = camera.id + "/proximity";
+        editor.putInt(key, camera.proximity.ordinal());
+        editor.apply();
+    }
+
+    public Set<String> readCameraBeaconsSetting(String cameraId) {
+        SharedPreferences settings = context.getApplicationContext().getSharedPreferences("CameraSwitch",0);
+        Set <String> beacons = settings.getStringSet(cameraId, null);
+        return beacons;
+    }
+
+    public void writeCameraBeaconsSetting(CameraModel camera) {
+        SharedPreferences settings = context.getApplicationContext().getSharedPreferences("CameraSwitch",0);
+        SharedPreferences.Editor editor = settings.edit();
+        String key = camera.id + "/beacons";
+        editor.putStringSet(key, camera.beacons);
         editor.apply();
     }
 }
